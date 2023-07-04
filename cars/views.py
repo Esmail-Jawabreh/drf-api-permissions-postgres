85dd220accf31efbba2ae23ba57f6d4a93e350b8
Esmail-Jawabreh <esmailjawabreh@gmail.com>
2023-07-04 23:21:29 +0000

    new changes

--- cars/views.py
+++ cars/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import Cars 
+from .models import Cars
 from .serializers import CarsSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .permissions import IsOwnerOrReadOnly
 
 # Create your views here.
+
 
 # ListAPIView
 class CarsList(generics.ListCreateAPIView):
     queryset = Cars.objects.all()
     serializer_class = CarsSerializer
+    permission_classes = [IsAuthenticated]
+
 
 # RetrieveAPIView RetrieveUpdateAPIView
 class CarsDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Cars.objects.all()
     serializer_class = CarsSerializer
+    permission_classes = [IsOwnerOrReadOnly]
